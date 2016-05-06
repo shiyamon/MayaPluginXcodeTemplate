fbdc9326c939e75eb46f4ee10bd8415a04f42453
吉田真也 <yoshida@mtblanc.jp>
2016-05-06 15:41:06 +0900

    the first commit.

---
+++ MayaPlugin.xctemplate/PluginMain.cpp
@@ -0,0 +1,27 @@
+// TM and (c) ___YEAR___ ___ORGANIZATIONNAME___. All Rights Reserved.
+// Reproduction in whole or in part without prior written permission of a
+// duly authorized representative is prohibited.
+#include <maya/MFnPlugin.h>
+
+MStatus initializePlugin(MObject plugin)
+{
+    MStatus status;
+    MFnPlugin pluginFn(plugin, "___ORGANIZATIONNAME___", "1.0.0", "Any");
+    
+    // Register elements here
+    
+    return status;
+}
+
+MStatus uninitializePlugin(MObject plugin)
+{
+    MStatus status;
+    MFnPlugin pluginFn(plugin);
+    
+    // Deregister elements here
+    
+    return status;
+}
+// TM and (c) ___YEAR___ ___ORGANIZATIONNAME___. All Rights Reserved.
+// Reproduction in whole or in part without prior written permission of a
+// duly authorized representative is prohibited.
